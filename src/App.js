81d64884c531415c284e31d5fb0d079b58f4b84a
Netanel <ne.zaffran@gmail.com>
2021-05-24 17:10:19 +0300

    minor change

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target='_blank'
           rel='noopener noreferrer'
         >
-          learn react
+          learn react ` updateddddddddd!!!!
         </a>
       </header>
     </div>
